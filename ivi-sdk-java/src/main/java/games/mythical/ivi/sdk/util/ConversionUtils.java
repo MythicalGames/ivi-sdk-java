423b395e8215d334e9a781b8518bd853ab651179
keith-miller <keith@mythical.games>
2021-02-05 17:12:27 -0800

    Fixing log message

--- ivi-sdk-java/src/main/java/games/mythical/ivi/sdk/util/ConversionUtils.java
+++ ivi-sdk-java/src/main/java/games/mythical/ivi/sdk/util/ConversionUtils.java
@@ -22,7 +22,7 @@ public class ConversionUtils {
             JsonFormat.parser().merge(metadata.toString(), structBuilder);
             return structBuilder.build();
         } catch (InvalidProtocolBufferException e) {
-            log.error("ConversionUtils: couldn't convert item properties!", e);
+            log.error("ConversionUtils: couldn't convert properties map!", e);
             throw new IVIException(IVIErrorCode.PARSING_DATA_EXCEPTION);
         }
     }
@@ -33,7 +33,7 @@ public class ConversionUtils {
             var propertiesString = JsonFormat.printer().print(properties);
             return objectMapper.readValue(propertiesString, new TypeReference<>() {});
         } catch (Exception  e) {
-            log.error("ConversionUtils: couldn't convert item properties!", e);
+            log.error("ConversionUtils: couldn't convert properties struct!", e);
             throw new IVIException(IVIErrorCode.PARSING_DATA_EXCEPTION);
         }
     }
