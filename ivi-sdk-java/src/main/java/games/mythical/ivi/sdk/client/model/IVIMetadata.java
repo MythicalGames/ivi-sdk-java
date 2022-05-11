5c9b841857d64387011e32454095e37a63c170a3
Jarett <jarettlear@gmail.com>
2022-05-11 11:32:26 -0500

    Feature/mgp 603 (#41)
    * adding animation url to java sdk to allow block party to send it over as its own entity just like the main image url
    
    * cleanup
    
    * proto
    
    Co-authored-by: Jarett Lear <jarett@mythical.games>

--- ivi-sdk-java/src/main/java/games/mythical/ivi/sdk/client/model/IVIMetadata.java
+++ ivi-sdk-java/src/main/java/games/mythical/ivi/sdk/client/model/IVIMetadata.java
@@ -5,6 +5,7 @@ import games.mythical.ivi.sdk.proto.common.Metadata;
 import games.mythical.ivi.sdk.util.ConversionUtils;
 import lombok.Builder;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Map;
 
@@ -14,6 +15,7 @@ public class IVIMetadata {
     private String name;
     private String description;
     private String image;
+    private String animationUrl;
     private Map<String, Object> properties;
 
     public static IVIMetadata fromProto(Metadata metadata) throws IVIException {
@@ -22,6 +24,7 @@ public class IVIMetadata {
                 .description(metadata.getDescription())
                 .image(metadata.getImage())
                 .properties(ConversionUtils.convertProperties(metadata.getProperties()))
+                .animationUrl(StringUtils.isNotEmpty(metadata.getAnimationUrl()) ? metadata.getAnimationUrl() : "")
                 .build();
     }
 
@@ -31,6 +34,7 @@ public class IVIMetadata {
                 .setDescription(metadata.getDescription())
                 .setImage(metadata.getImage())
                 .setProperties(ConversionUtils.convertProperties(metadata.getProperties()))
+                .setAnimationUrl(StringUtils.isNotEmpty(metadata.getAnimationUrl()) ? metadata.getAnimationUrl() : "")
                 .build();
     }
 }
