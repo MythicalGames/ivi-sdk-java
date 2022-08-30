96926a02c8ac10a9181d622752e1e7d2324c0de7
Jarett <jarettlear@gmail.com>
2022-08-30 14:12:18 -0500

    cleaning up order request for gr4vy (#52)
    Co-authored-by: Jarett Lear <jarett@mythical.games>

--- ivi-sdk-java/src/main/java/games/mythical/ivi/sdk/client/IVIOrderClient.java
+++ ivi-sdk-java/src/main/java/games/mythical/ivi/sdk/client/IVIOrderClient.java
@@ -189,20 +189,10 @@ public class IVIOrderClient extends AbstractIVIClient {
     }
 
     public IVIFinalizeOrderResponse finalizeGr4vyOrder(String orderId,
-                                                       String cardType,
-                                                       String expMonth,
-                                                       String expYear,
-                                                       String instrumentId,
-                                                       String securityCode,
                                                        String paymentMethodTokenId) throws IVIException {
 
         var paymentData = PaymentRequestProto.newBuilder()
                 .setGr4Vy(Gr4vyPaymentRequestProto.newBuilder()
-                        .setCardType(cardType)
-                        .setExpMonth(expMonth)
-                        .setExpYear(expYear)
-                        .setInstrumentId(instrumentId)
-                        .setSecurityCode(securityCode)
                         .setPaymentMethodTokenId(paymentMethodTokenId)
                         .build())
                 .build();
